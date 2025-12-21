44792c2aae95edb64b361272c4757519286a46b1
Egor Rotinov <rotinov-github@pm.me>
2025-12-21 22:18:28 +0500

    fix: equality operator fix

--- src/main/java/io/veridia/jsonlogic/operators/EqualityOperator.java
+++ src/main/java/io/veridia/jsonlogic/operators/EqualityOperator.java
@@ -3,7 +3,6 @@ package io.veridia.jsonlogic.operators;
 import io.veridia.jsonlogic.CompiledExpression;
 import io.veridia.jsonlogic.Operator;
 import io.veridia.jsonlogic.helpers.ToBoolean;
-import io.veridia.jsonlogic.helpers.ToDouble;
 
 import java.util.List;
 
@@ -27,21 +26,16 @@ public class EqualityOperator implements Operator {
             }
 
             // Check numeric loose equality
-            if (
-                    (left instanceof Number && right instanceof Number)
-                            || (left instanceof Number && right instanceof String)
-                            || (left instanceof String && right instanceof Number)
-            ) {
-                return Double.valueOf(ToDouble.eval(left)).equals(ToDouble.eval(right));
+            if (left instanceof Number && right instanceof Number) {
+                return Double.valueOf(((Number) left).doubleValue()).equals(((Number) right).doubleValue());
             }
 
-            if (
-                    (left instanceof Number && right instanceof Boolean)
-                            || (left instanceof Boolean && right instanceof Number)
-                            || (left instanceof String && right instanceof Boolean)
-                            || (left instanceof Boolean && right instanceof String)
-            ) {
-                return ToBoolean.eval(left) == ToBoolean.eval(right);
+            if (left instanceof Number && right instanceof String) {
+                return compareNumberToString((Number) left, (String) right);
+            }
+
+            if (left instanceof Number && right instanceof Boolean) {
+                return compareNumberToBoolean((Number) left, (Boolean) right);
             }
 
             // Check string loose equality
@@ -49,12 +43,53 @@ public class EqualityOperator implements Operator {
                 return left.equals(right);
             }
 
+            if (left instanceof String && right instanceof Number) {
+                return compareNumberToString((Number) right, (String) left);
+            }
+
+            if (left instanceof String && right instanceof Boolean) {
+                return compareStringToBoolean((String) left, (Boolean) right);
+            }
+
             // Check boolean loose equality
             if (left instanceof Boolean && right instanceof Boolean) {
                 return ((Boolean) left).booleanValue() == ((Boolean) right).booleanValue();
             }
 
+            if (left instanceof Boolean && right instanceof Number) {
+                return compareNumberToBoolean((Number) right, (Boolean) left);
+            }
+
+            if (left instanceof Boolean && right instanceof String) {
+                return compareStringToBoolean((String) right, (Boolean) left);
+            }
+
             return !ToBoolean.eval(left) && !ToBoolean.eval(right);
         };
+    }
+
+    private boolean compareNumberToString(Number left, String right) {
+        try {
+            if (right.trim().isEmpty()) {
+                right = "0";
+            }
+
+            return Double.parseDouble(right) == left.doubleValue();
+        }
+        catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    private boolean compareNumberToBoolean(Number left, Boolean right) {
+        if (right) {
+            return left.doubleValue() == 1.0;
+        }
+
+        return left.doubleValue() == 0.0;
+    }
+
+    private boolean compareStringToBoolean(String left, Boolean right) {
+        return ToBoolean.eval(left) == right;
     }
 }
